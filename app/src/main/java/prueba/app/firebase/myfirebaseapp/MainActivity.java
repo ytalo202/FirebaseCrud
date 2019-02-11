7c06bc107637a43d5b600702d2a0c088a3b45d5f
ytalo <tuestaytalo@gmail.com>
2019-02-11 15:57:45 -0500

    Initial commit

---
+++ app/src/main/java/prueba/app/firebase/myfirebaseapp/MainActivity.java
@@ -0,0 +1,178 @@
+package prueba.app.firebase.myfirebaseapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import prueba.app.firebase.myfirebaseapp.model.Persona;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Persona> listPerson = new ArrayList<Persona>();
+    ArrayAdapter<Persona> arrayAdapterPersona;
+
+    EditText nomP, appP,correoP,passwordP;
+    ListView listV_personas;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    Persona personaSelected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nomP = findViewById(R.id.txt_nombrePersona);
+        appP = findViewById(R.id.txt_appPersona);
+        correoP = findViewById(R.id.txt_correoPersona);
+        passwordP = findViewById(R.id.txt_passwordPersona);
+
+        listV_personas = findViewById(R.id.lv_datosPersonas);
+        inicializarFirebase();
+        listarDatos();
+
+        listV_personas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                personaSelected = (Persona) parent.getItemAtPosition(position);
+                nomP.setText(personaSelected.getNombre());
+                appP.setText(personaSelected.getApellido());
+                correoP.setText(personaSelected.getCorreo());
+                passwordP.setText(personaSelected.getPassword());
+            }
+        });
+
+    }
+
+    private void listarDatos() {
+        databaseReference.child("Persona").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listPerson.clear();
+                for (DataSnapshot objSnaptshot : dataSnapshot.getChildren()){
+                    Persona p = objSnaptshot.getValue(Persona.class);
+                    listPerson.add(p);
+
+                    arrayAdapterPersona = new ArrayAdapter<Persona>(MainActivity.this, android.R.layout.simple_list_item_1, listPerson);
+                    listV_personas.setAdapter(arrayAdapterPersona);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        //firebaseDatabase.setPersistenceEnabled(true);
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String nombre = nomP.getText().toString();
+        String correo = correoP.getText().toString();
+        String password = passwordP.getText().toString();
+        String app = appP.getText().toString();
+
+        switch (item.getItemId()){
+            case R.id.icon_add:{
+                if (nombre.equals("")||correo.equals("")||password.equals("")||app.equals("")){
+                    validacion();
+                }
+                else {
+                    Persona p = new Persona();
+                    p.setUid(UUID.randomUUID().toString());
+                    p.setNombre(nombre);
+                    p.setApellido(app);
+                    p.setCorreo(correo);
+                    p.setPassword(password);
+                    databaseReference.child("Persona").child(p.getUid()).setValue(p);
+                    Toast.makeText(this, "Agregado", Toast.LENGTH_LONG).show();
+                    limpiarCajas();
+                }
+                break;
+            }
+            case R.id.icon_save:{
+                Persona p = new Persona();
+                p.setUid(personaSelected.getUid());
+                p.setNombre(nomP.getText().toString().trim());
+                p.setApellido(appP.getText().toString().trim());
+                p.setCorreo(correoP.getText().toString().trim());
+                p.setPassword(passwordP.getText().toString().trim());
+                databaseReference.child("Persona").child(p.getUid()).setValue(p);
+                Toast.makeText(this,"Actualizado", Toast.LENGTH_LONG).show();
+                limpiarCajas();
+                break;
+            }
+            case R.id.icon_delete:{
+                Persona p = new Persona();
+                p.setUid(personaSelected.getUid());
+                databaseReference.child("Persona").child(p.getUid()).removeValue();
+                Toast.makeText(this,"Eliminado", Toast.LENGTH_LONG).show();
+                limpiarCajas();
+                break;
+            }
+            default:break;
+        }
+        return true;
+    }
+
+    private void limpiarCajas() {
+        nomP.setText("");
+        correoP.setText("");
+        passwordP.setText("");
+        appP.setText("");
+    }
+
+    private void validacion() {
+        String nombre = nomP.getText().toString();
+        String correo = correoP.getText().toString();
+        String password = passwordP.getText().toString();
+        String app = appP.getText().toString();
+        if (nombre.equals("")){
+            nomP.setError("Required");
+        }
+        else if (app.equals("")){
+            appP.setError("Required");
+        }
+        else if (correo.equals("")){
+            correoP.setError("Required");
+        }
+        else if (password.equals("")){
+            passwordP.setError("Required");
+        }
+    }
+}
---
+++ app/src/main/java/prueba/app/firebase/myfirebaseapp/model/Persona.java
@@ -0,0 +1,59 @@
+package prueba.app.firebase.myfirebaseapp.model;
+
+/**
+ * Created by Angel on 17/08/2018.
+ */
+
+public class Persona {
+
+    private String uid;
+    private String Nombre;
+    private String Apellido;
+    private String Correo;
+    private String Password;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+}
